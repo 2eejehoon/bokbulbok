c2930081eb61d977061e700e68a461e1cd392ead
leejehoon92 <2eejehoon@gmail.com>
2023-02-25 17:28:52 +0900

    룰렛 컴포넌트 제작

--- src/components/common/Roulette/Roulette.tsx
+++ src/components/common/Roulette/Roulette.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 import classNames from "classnames/bind";
 import Button from "../Button/Button";
 import style from "./Roulette.module.scss";
@@ -10,32 +10,55 @@ interface RouletteProps {
 }
 
 export default function Roulette({ data }: RouletteProps) {
-  const [spin, setSpin] = useState("");
+  const [start, setStart] = useState("");
   const [stop, setStop] = useState("");
+  const [spin, setSpin] = useState(false);
+
+  // 데이터의 길이에 따라 룰렛의 CSS 효과를 변경합니다.
+  const length = useMemo(() => {
+    const len = data.length;
+    if (len === 1) return "one";
+    if (len === 2) return "two";
+    if (len === 3) return "three";
+    if (len === 4) return "four";
+    if (len === 5) return "five";
+    if (len === 6) return "six";
+  }, [data]);
 
   const handleClick = useCallback(() => {
     setStop((prev) => "");
-    setSpin((prev) => "spin");
-    setTimeout(
-      () => setStop((prev) => "stop"),
-      Math.floor(Math.random() * 5000) + 1000
-    );
+    setStart((prev) => "start");
+    setSpin((prev) => true);
+    setTimeout(() => {
+      setStop((prev) => "stop");
+      setSpin((prev) => false);
+    }, Math.floor(Math.random() * 5000) + 1000);
   }, []);
   return (
     <>
       <div className={style.arrow} />
-      <ul className={cx("roulette", spin, stop)}>
+      <ul className={cx("roulette", start, stop)}>
         {data.map((title) => {
           return (
-            <li key={title} className={style.li}>
-              <div className={style.text}>{title}</div>
+            <li key={title} className={cx("li", length)}>
+              <div className={cx("text", length)}>{title}</div>
             </li>
           );
         })}
       </ul>
-      <Button type="button" color="black" size="medium" onClick={handleClick}>
-        돌려 돌려 돌림판
-      </Button>
+      {/* 룰렛이 회전하고 있을 때 버튼이 보이지 않습니다.*/}
+      {!spin && (
+        <div className={style.buttonContainer}>
+          <Button
+            type="button"
+            color="black"
+            size="medium"
+            onClick={handleClick}
+          >
+            돌려 돌려 돌림판
+          </Button>
+        </div>
+      )}
     </>
   );
 }
