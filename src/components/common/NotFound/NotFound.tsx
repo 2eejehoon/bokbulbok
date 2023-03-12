f789a5d0148d453dfdc2b2cc2922f48a1ffab7a4
leejehoon92 <2eejehoon@gmail.com>
2023-03-12 15:25:44 +0900

    fix: vercel 환경 변수 설정

--- src/components/common/NotFound/NotFound.tsx
+++ src/components/common/NotFound/NotFound.tsx
@@ -1,6 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import style from "./NotFound.module.scss";
 
 export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    window.setTimeout(() => router.push("/"), 1000);
+  });
   return (
     <div className={style.container}>
       <h1 className={style.title}>404</h1>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,8 @@
 import { ReactElement } from "react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import BaseLayout from "@/components/common/Layout/BaseLayout/BaseLayout";
 import NotFound from "@/components/common/NotFound/NotFound";
 
 export default function NotFoundPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    window.setTimeout(() => router.push("/"), 1000);
-  });
-
   return <NotFound />;
 }
 
