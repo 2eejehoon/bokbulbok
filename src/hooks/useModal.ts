3fa6b3ac3b6cc60de823975e5a1b0da0b1c481c8
leejehoon92 <2eejehoon@gmail.com>
2023-03-25 15:55:17 +0900

    refactor: useModal 훅 리턴 타입 지정

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,10 +1,16 @@
 import { useState, useCallback } from "react";
 
-export default function useModal() {
+type useModalReturnType = [
+  modalOpen: boolean,
+  handleModalOpen: () => void,
+  handleModalClose: () => void
+];
+
+export default function useModal(): useModalReturnType {
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleModalOpen = useCallback(() => setModalOpen(true), []);
   const handleModalClose = useCallback(() => setModalOpen(false), []);
 
-  return [modalOpen, handleModalOpen, handleModalClose] as const;
+  return [modalOpen, handleModalOpen, handleModalClose];
 }
