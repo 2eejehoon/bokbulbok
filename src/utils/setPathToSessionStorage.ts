6394b04912443aa8de60c6fcc7e3e11f9c2117c5
leejehoon92 <2eejehoon@gmail.com>
2023-04-05 19:01:04 +0900

    chore: setPathToSessionStorage 함수에 console.log 작성

--- src/utils/setPathToSessionStorage.ts
+++ src/utils/setPathToSessionStorage.ts
@@ -3,11 +3,13 @@ export const setPathToSessionStorage = () => {
   if (!storage) return;
 
   const prevPath = storage.getItem("currentPath");
+  console.log(prevPath, "prev");
   storage.setItem("prevPath", prevPath as string);
 
   const pathName = globalThis.location.search
     ? globalThis.location.pathname + globalThis.location.search
     : globalThis.location.pathname;
 
+  console.log(pathName, "current");
   storage.setItem("currentPath", pathName);
 };
