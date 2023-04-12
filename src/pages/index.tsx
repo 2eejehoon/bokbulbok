af3d3af671c8717607a767cbf307172a43032f00
leejehoon92 <2eejehoon@gmail.com>
2023-04-12 12:52:34 +0900

    feat: index 페이지 Seo 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { useGeolocation } from "@/hooks/useGeolocation";
 import useCustomRouter from "@/hooks/useCustomRouter";
 import Loading from "@/components/common/Loading/Loading";
+import Seo from "@/components/common/Seo/Seo";
 
 export default function Home() {
+  const router = useRouter();
   const location = useGeolocation();
   const customRouterPush = useCustomRouter();
 
@@ -13,5 +16,10 @@ export default function Home() {
     customRouterPush("location", location);
   }, [location]);
 
-  return <Loading />;
+  return (
+    <>
+      <Seo title={"복불복"} description={"현재 위치 음식점 정보"} url={router.asPath} />
+      <Loading />
+    </>
+  );
 }
