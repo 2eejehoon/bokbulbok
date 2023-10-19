935643f3f6ccec073b5c11bb33476d3d16b0660c
leejehoon <2eejehoon@gmail.com>
2023-10-19 15:52:15 +0900

    불필요한 함수 삭제

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -26,43 +26,6 @@ export const convertCategoryToText = (category?: string) => {
   }
 };
 
-export const convertImageIndexToText = (index: string) => {
-  switch (index) {
-    case "1":
-      return "one";
-
-    case "2":
-      return "two";
-
-    case "3":
-      return "three";
-
-    case "4":
-      return "four";
-
-    case "5":
-      return "five";
-
-    case "6":
-      return "six";
-
-    case "7":
-      return "seven";
-
-    case "8":
-      return "eight";
-
-    case "9":
-      return "nine";
-
-    case "10":
-      return "ten";
-
-    default:
-      return "one";
-  }
-};
-
 export const convertLengthToText = (length?: number) => {
   switch (length) {
     case 0:
