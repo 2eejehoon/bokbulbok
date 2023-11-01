1345da16472abc4249449f4dea0f36053f334575
leejehoon <2eejehoon@gmail.com>
2023-11-01 13:44:01 +0900

    컨버트 유틸 함수 수정

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,57 +1,31 @@
-export const convertCategoryToText = (category?: string) => {
-  switch (category) {
-    case "A05020100":
-      return "한식";
+export const CATEGORY_TO_TEXT = {
+  A05020100: "한식",
+  A05020200: "양식",
+  A05020300: "일식",
+  A05020400: "중식",
+  A05020700: "이색음식",
+  A05020900: "카페",
+  A05021000: "클럽",
+} as const;
 
-    case "A05020200":
-      return "양식";
-
-    case "A05020300":
-      return "일식";
-
-    case "A05020400":
-      return "중식";
-
-    case "A05020700":
-      return "이색음식";
-
-    case "A05020900":
-      return "카페";
-
-    case "A05021000":
-      return "클럽";
-
-    default:
-      return "기타";
-  }
+export const convertCategoryToText = (
+  category?: keyof typeof CATEGORY_TO_TEXT
+) => {
+  return category ? CATEGORY_TO_TEXT[category] : "기타";
 };
 
-export const convertLengthToText = (length?: number) => {
-  switch (length) {
-    case 0:
-      return "zero";
+export const LENGTH_TO_TEXT = {
+  0: "zero",
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+} as const;
 
-    case 1:
-      return "one";
-
-    case 2:
-      return "two";
-
-    case 3:
-      return "three";
-
-    case 4:
-      return "four";
-
-    case 5:
-      return "five";
-
-    case 6:
-      return "six";
-
-    default:
-      return "zero";
-  }
+export const convertLengthToText = (length?: keyof typeof LENGTH_TO_TEXT) => {
+  return length ? LENGTH_TO_TEXT[length] : "zero";
 };
 
 export const convertBrToSpace = (text?: string) => {
@@ -62,38 +36,22 @@ export const convertRangeToQuery = (range: number) => {
   return range * 1000;
 };
 
-export const convertSortToQuery = (sort: string) => {
-  switch (sort) {
-    case "제목순":
-      return "A";
+export const SORT_TO_QUERY = {
+  제목순: "A",
+  수정순: "B",
+  등록순: "D",
+} as const;
 
-    case "수정순":
-      return "B";
+export const convertSortToQuery = (sort: keyof typeof SORT_TO_QUERY) => {
+  return SORT_TO_QUERY[sort] ?? "D";
+};
 
-    case "등록순":
-      return "D";
+export const QUERY_TO_SORT = { A: "제목순", B: "수정순", D: "등록순" } as const;
 
-    default:
-      return "D";
-  }
+export const convertQueryToSort = (query: keyof typeof QUERY_TO_SORT) => {
+  return QUERY_TO_SORT[query] ?? "등록순";
 };
 
 export const convertQueryToRange = (query: string) => {
   return Number(query) / 1000;
-};
-
-export const convertQueryToSort = (query: string) => {
-  switch (query) {
-    case "A":
-      return "제목순";
-
-    case "B":
-      return "수정순";
-
-    case "D":
-      return "등록순";
-
-    default:
-      return "D";
-  }
 };
